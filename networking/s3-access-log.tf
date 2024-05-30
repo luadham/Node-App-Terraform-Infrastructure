d3f04289239107e5f341e58581467754b0651b03
Adham Adel <luadham@gmail.com>
2024-05-31 01:12:00 +0300

    Role for LB Access S3

--- networking/s3-access-log.tf
+++ networking/s3-access-log.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "lb-access-logs" {
+resource "aws_s3_bucket" "lb_access_logs" {
   bucket = "lb-access-log-${random_id.random_number.hex}"
   tags = {
     Name = "LB-Access-Log"
