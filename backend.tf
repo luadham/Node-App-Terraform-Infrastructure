2e160601cddd5489f36a2293db7ef2ad10471ed2
Adham Adel <luadham@gmail.com>
2024-05-31 00:17:26 +0300

    Add Backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "mybucket"
+    bucket         = "adham-terraform-state-us-east-1"
     key            = "node-app-tfstate.tfstate"
     region         = "us-east-1"
-    dynamodb_table = ""
+    dynamodb_table = "terraform-lock-table"
   }
 }
